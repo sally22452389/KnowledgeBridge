8cb6fb6f1582fae7de77b3c26626dc43ddce3bc4
(っ◔◡◔)っ                     ♥ мαρℓєMapleyiуι ♥ <mapleyi@MapleyiMacBook-Air.local>
2023-05-16 19:21:09 +0800

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import HomeComponent from "./components/home-components";
+import NavComponent from "./components/nav-component";
+import RegisterComponent from "./components/register-component";
+
+function App() {
+  return (
+    <div>
+      <NavComponent />
+      <Switch>
+        <Route path="/" exact>
+          <HomeComponent />
+        </Route>
+        <Route path="/register" exact>
+          <RegisterComponent />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
