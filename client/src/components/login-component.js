28aacadf36c9fdb44cbae0098fe9d97c7511a48a
(っ◔◡◔)っ                     ♥ мαρℓєMapleyiуι ♥ <mapleyi@MapleyiMacBook-Air.local>
2023-05-29 18:03:43 +0800

    Add login

---
+++ client/src/components/login-component.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import AuthService from "../services/auth.services";
+
+const LoginComponent = () => {
+  const history = useHistory();
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [message, setMessage] = useState("");
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  // 這個函數會調用AuthService的login函數，並將email和password作為參數傳入
+  // 它會在localStorage中保存用戶的token，如果存在的話
+  // 最後，它將使用history對象將用戶重定向到profile頁面
+  const handleLogin = () => {
+    AuthService.login(email, password)
+      .then((response) => {
+        if (response.data.token) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+        history.push("/profile");
+      })
+      .catch((error) => {
+        console.log(error.response);
+        setMessage(error.response.data);
+      });
+  };
+
+  return (
+    <div style={{ padding: "3rem" }} className="col-md-12">
+      <div>
+        {message && (
+          <div className="alert alert-danger" role="alert">
+            {message}
+          </div>
+        )}
+        <div className="form-group">
+          <label htmlFor="username">Email</label>
+          <input
+            onChange={handleChangeEmail}
+            type="text"
+            className="form-control"
+            name="email"
+          />
+        </div>
+        <br />
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            onChange={handleChangePassword}
+            type="password"
+            className="form-control"
+            name="password"
+          />
+        </div>
+        <br />
+        <div className="form-group">
+          <button onClick={handleLogin} className="btn btn-primary btn-block">
+            <span>Login</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginComponent;
